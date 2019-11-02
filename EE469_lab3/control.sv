b2fd6ef1e95c76ea2d14d9a83251a03bb7b920f9
Isfan G <16isfang@gmail.com>
2019-11-02 01:15:14 -0700

    Instr BL implemented, control cases complete

--- EE469_lab3/control.sv
+++ EE469_lab3/control.sv
@@ -8,13 +8,11 @@ module control (clk, reset);
 	output logic Reg2Loc, ALUSrc, MemToReg, RegWrite, MemWrite, BrTaken, UncondBr;
 	output logic [2:0] = ALUOp;
 
-
 	//flag_wr_en stores flags to the flag register
 	//rd_x30 used for the purposes of Instr BL
+	//pc_rd used to choose between PC logic or PC = Reg[Rd]
+	output logic flag_wr_en, rd_x30, pc_rd;
 
-	output logic flag_wr_en, rd_x30;
-
-	
 	logic [10:0] opcode;
 	logic [9:0] ctrl;
 	
@@ -40,21 +38,25 @@ module control (clk, reset);
 		11'b1001000100x: begin
 							ctrl = 10'b110100x010; // ADDI - 0x244 (10bit)
 							rd_x30 = 0;
+							pc_rd = 0;
 							flag_wr_en = 0; 
 						 end
 		11'b10101011000: begin
 							ctrl = 10'b100100x010;// ADDS - 0x558 (11bit)  
 							rd_x30 = 0;
+							pc_rd = 0;
 							flag_wr_en = 1; 
 						 end
 		11'b000101xxxxx: begin
 							ctrl = 10'bxxx0011xxx; // B - 0x05 (6bit)
 							rd_x30 = 0;
+							pc_rd = 0;
 							flag_wr_en = 0; 
 						 end
 		11'b01010100xxx: begin
 							ctrl = {5'b00x00, (alu_neg && (alu_neg != alu_overf)), 4'b000} // B.LT - 0x54 (8bit)
 							rd_x30 = 0;
+							pc_rd = 0;
 							flag_wr_en = 0; 
 						 end
 
@@ -65,31 +67,41 @@ module control (clk, reset);
 		11'b100101xxxxx: begin
 							ctrl = 10'bxxx1011xxx; // BL - 0x25 (6bit)
 							rd_x30 = 1;
+							pc_rd = 0;
 							flag_wr_en = 0; 
 						 end
+
+		//Mux needed to choose between
+		//PC in register RD or PC+4
+		//controlled by pc_rd;
 		11'b11010110000: begin
-							ctrl = 10'b // BR - 0x6B0 (11bit)
+							ctrl = 10'b0xx00xxxxx; // BR - 0x6B0 (11bit)
 							rd_x30 = 0;
+							pc_rd = 0;
 							flag_wr_en = 0; 
 						 end
 		11'b10110100xxx: begin
 							ctrl = {5'b00x00, alu_zero, 4'b0000}; // CBZ - 0xB4 (8bit)
 							rd_x30 = 0;
+							pc_rd = 1;
 							flag_wr_en = 0; 
 						 end
 		11'b11111000010: begin
 							ctrl = 10'bx11100x010; // LDUR =- 0x7C2 (11bit)
 							rd_x30 = 0;
+							pc_rd = 0;
 							flag_wr_en = 0; 
 						 end
 		11'b11111000000: begin
 							ctrl = 10'b01x010x010; // STUR - 0x7C0 (11bit)
 							rd_x30 = 0;
+							pc_rd = 0;
 							flag_wr_en = 0; 
 						 end
 		11'b11101011000: begin
 							ctrl = 10'b100100x011; // SUBS - 0x658 (11bit)
 							rd_x30 = 0;
+							pc_rd = 0;
 							flag_wr_en = 1;
 						end
 		endcase

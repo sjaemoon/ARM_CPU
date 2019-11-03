7a94fcb9c8b6763bf037041c7175e865041e6560
Seungjae Moon <bmoon9@uw.edu>
2019-11-02 18:04:33 -0700

    Completed wiring for the datapath and changed i/o of processor

---
+++ EE469_lab3/datapath.sv
@@ -0,0 +1,76 @@
+`timescale 1ns/10ps
+module datapath (clk, Rd, Rm, Rn, PCPlusFour, X30, DAddr9, ALUImm12, 
+					Reg2Loc, ALUSrc, MemToReg, RegWrite, MemWrite, ALUOp, Rd_X30, 
+					flag_neg, flag_zero, flag_overf, flag_cOut, Db_ext);
+	
+	input clk;	
+	input logic [63:0] Rd, Rm, Rn, PCPlusFour, X30, DAddr9, ALUImm12;
+	input logic Reg2Loc, ALUSrc, MemToReg, RegWrite, MemWrite, Rd_X30;
+	input logic [2:0] ALUOp;
+	
+	// inputs/outputs of RegFile
+	logic [63:0] Aw, Aa, Ab, Dw, Da, Db;
+	// i/o of DataMemory
+	logic [63:0] Din, mem_Addr, Dout;
+	// i/o of muxes and alu
+	logic [1:0][63:0] Reg2Loc_in0, Reg2Loc_in1, Rd_X30_in0, Rd_X30_in1, ALUSrc_in, MemToReg_in;
+	logic [63:0] Reg2Loc_out0, Reg2Loc_out1, Rd_X30_out0, Rd_X30_out1, ALUSrc_out, MemToReg_out, ALUOp_out;
+	logic negative, zero, overflow, carry_out;
+	
+	// sign extend DAddr9 and ALUImm12
+	input logic [63:0] DAddr9_se, ALUImm12_se;
+	sign_extend daddr9_se #(.WIDTH(9)) (.in(DAddr9), .out(DAddr9_se));	
+	sign_extend aluimm12_se ##(.WIDTH(12)) (.in(ALUImm12), .out(ALUImm12_se));
+		
+	// set up input of the muxes
+	assign Reg2Loc_in0[0] = Rd;
+	assign Reg2Loc_in0[1] = Rn;
+	
+	assign Rd_X30_in0[0] = Rd;
+	assign Rd_X30_in0[1] = X30;
+	
+	assign Reg2Loc_in1[0] = DAddr9_se;
+	assign Reg2Loc_in1[1] = ALUimm12_se;
+	
+	assign ALUSrc_in[0] = Db;
+	assign ALUSrc_in[1] = Reg2Loc_out1;
+
+	assign MemToReg_in[0] = ALUOp_out;
+	assign MemToReg_in[1] = mem_Addr;
+	
+	assign Rd_X30_in1[0] = PCPlusFour;
+	assign Rd_X30_in1[0] = MemToReg_out;
+	
+	// instantiation of muxes and alu
+	mux2_1 #(.WIDTH(64)) reg2loc_mux0 (.in(Reg2Loc_in0), .sel(Reg2Loc), .out(Reg2Loc_out0));
+	mux2_1 #(.WIDTH(64)) rd_x30_mux0 (.in(Rd_X30_in0), .sel(Rd_X30), .out(Rd_X30_out0));
+	mux2_1 #(.WIDTH(64)) reg2loc_mux1 (.in(Reg2Loc1_in),.sel(Reg2Loc), .out(Reg2Loc1_out));
+	mux2_1 #(.WIDTH(64)) alusrc_mux (.in(ALUSrc_in),.sel(ALUSrc), .out(ALUSrc_out));
+	alu aluop_alu (.A(Da), .B(ALUSrc_out), .cntrl(ALUOp), .result(ALUOp_out), .negative, .zero, .overflow, .carry_out);
+	mux2_1 #(.WIDTH(64)) memtoreg_mux (.in(MemToReg_in),.sel(MemToReg), .out(MemToReg_out));
+	mux2_1 #(.WIDTH(64)) rd_x30_mux1 (.in(Rd_X30_in1), .sel(Rd_X30), .out(Rd_X30_out1));
+	
+	// instantitation of RegFile
+	assign Aw = Rd_X30_out0;
+	assign Aa = Rn;
+	assign Ab = Reg2Loc_out0;
+	assign Dw = Rd_X30_Out1;
+	regfile rf (.ReadData1(Da), .ReadData2(Db), .WriteData(Wd), 
+					.ReadRegister1(Aa),. ReadRegister2(Ab), 
+					.WriteRegister(Aw), .RegWrite, .clk);
+				
+	// instantiation of DataMemory
+	assign Din = Db;
+	assign mem_Addr = ALUOp_out;
+	datamem dm (.address(mem_Addr), .write_enable(MemWrite), .read_enable(1'b1), 
+					.write_data(Db), .clk, .xfer_size(4'd8), .read_data(Dout));
+	
+	// assign datapath outputs
+	assign flag_neg = negative;
+	assign flag_zero = zero;
+	assign flag_overf = overflow;
+	assign flag_cOut = carry_out;
+	assign Db_ext = Db;
+	
+	
+endmodule
--- EE469_lab3/processor.sv
+++ EE469_lab3/processor.sv
@@ -6,26 +6,21 @@ module processor (clk, reset);
 	logic [63:0] pc_Addr;
 	logic [31:0] instruction;
 	
-	logic [63:0] Rd, Rm, Rn;
-	logic [63:0] Aw, Ab, Aa, reg_WrEn, Dw, Da, Db;
-	logic [63:0] Din, mem_Addr, mem_WrEn, Dout;
-	
-	logic flag_neg, flag_zero, flag_overf, flag_cOut;
+	logic Rd, Rm, Rn, PCPlusFour, X30, DAddr9, ALUImm12;
 	logic Reg2Loc, ALUSrc, MemToReg, RegWrite, MemWrite, BrTaken, UncondBr;
 	logic [2:0] ALUOp;
+	logic flag_neg, flag_zero, flag_overf, flag_cOut;
 	
- 	program_counter pc (.clk, .reset, .pc_ext(Db), .BrTaken, .UncondBr, .CondAddr19, .BrAddr26, .pc_out(pc_Addr));
+	
+ 	program_counter pc (.clk, .reset, .pc_ext(Db_ext), .BrTaken, .UncondBr, .CondAddr19, .BrAddr26, .pc_out(pc_Addr));
 	
 	instructmem instr (.address(pc_address), .instruction, .clk);
 	
-	control (.flag_neg, .flag_zero, .flag_overf, .flag_cOut, .instruction,
+	control ctrl (.opcode, .flag_neg, .flag_zero, .flag_overf, .flag_cOut, 
 				.Reg2Loc, .ALUSrc, .MemToReg, .RegWrite, .MemWrite, .BrTaken, .UncondBr, .ALUOp);
 	
-	assign reg_WrEn = RegWrite;
-	regfile(.ReadData1(Da), .ReadData2(Db), .WriteData(Wd), 
-				.ReadRegister1(Aa),. ReadRegister2(Ab), 
-				.WriteRegister(Aw), .RegWrite(reg_WrEn), .clk);
-				
-	datapath(.clk, .Da, .Db, .Reg2Loc, .ALUSrc, .MemToReg, .RegWrite, .MemWrite, .ALUOp, .Dw);
+	datapath dp (.clk, .Rd, .Rm, .Rn, .PCPlusFour, .X30, .DAddr9, .ALUImm12, 
+					.Reg2Loc, .ALUSrc, .MemToReg, .RegWrite, .MemWrite, .ALUOp, .Rd_X30, 
+					.flag_neg, .flag_zero, .flag_overf, .flag_cOut, .Db_ext);
 
 endmodule
